3d54ed5a5b0a196095308a8963b085dc4e2f3a68
ali <ali_babaie2005@yahoo.com>
2024-11-26 19:17:42 +0330

    section1 session#2

--- Scripts.js
+++ Scripts.js
@@ -1,1 +1,27 @@
-console.log("conected...");
+
+// var number;
+// var Name;
+
+// number=1;
+// Name=" a l i ";
+
+// var number,Name,LastName,X,Y;
+
+var a,b,c;
+
+a = 12;
+b = 8;
+// c = a + b;
+// c = a - b;
+// c = a * b;
+// c = a / b;
+c = a % b;
+
+// console.log(Name);
+
+document.write(c);
+
+// window.alert(Name);
+
+
+
