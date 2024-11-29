bf54daa05cb88e37fa6a62616ef15a0048035b4b
ali <ali_babaie2005@yahoo.com>
2024-11-29 14:07:37 +0330

    section4 session8

--- Scripts.js
+++ Scripts.js
@@ -1,14 +1,47 @@
+//Named Functions....
+
 function findBiggestFraction(a,b)
 {
 
-a>b ? console.log("a:"+a) : console.log("b:"+b);
+var result;
+a>b ? result=["firstFraction",a] : result=["secondFraction",b];
+return result;
 
 }
 
-var firstFraction=3/4;
+//var newResult = findBiggestFraction(7/16,13/25);
 
-var secondFraction=5/7;
+//console.log(findBiggestFraction(7/16,13/25));
+
+var fractionResult = findBiggestFraction(7/16,13/25);
+
+//console.log("Fraction "+fractionResult[0]+" with a value "+fractionResult[1]+" is the Biggest");
 
 
-findBiggestFraction(firstFraction,secondFraction);
-findBiggestFraction(7/16,13/25);
+
+//Anonymous Functions....
+
+var theBiggest= function (a,b){
+    var result;
+    a>b ? result=["a",a] : result=["b",b];
+    return result;
+}
+//console.log(theBiggest(7/9,13/24));
+//console.log(theBiggest);
+//console.log(theBiggest());
+
+
+
+//Immediately Invoked Functions....
+
+var theBiggest= (function (a,b){
+
+    var result;
+    a>b ? result=["a",a] : result=["b",b];
+    return result;
+
+})(7/9,13/24);
+
+console.log(theBiggest);
+
+
