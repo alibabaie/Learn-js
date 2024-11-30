b77ea6c2bc9a72ff675c38154c6d21d8b4781db4
ali <ali_babaie2005@yahoo.com>
2024-11-30 14:18:26 +0330

    section7 session12

--- Scripts.js
+++ Scripts.js
@@ -1,48 +1,15 @@
-// var person={
-//     firstName:"Ali",
-//     lastName:"Babaie"
-// }
-// var person2=new Object();
+const Cars=["Benz","BMW","Peykan","Pride","MVM"];
 
-// person2.phoneNumber=9119037444;
-// person2.Age=28;
-// console.log(person2);
+// console.log(Cars);
 
-var course={
-    title:"JavaScript",
-    teacher:"Mohammad Hashemi",
-    level:1,
-    isActive:true,
-    views:0,
-    updateView:function(){
-        return ++course.views;
-    }
+// console.log("The Car Brand Is :"+Cars[0]);
+// console.log("The Car Brand Is :"+Cars[1]);
+// console.log("The Car Brand Is :"+Cars[2]);
+// console.log("The Car Brand Is :"+Cars[3]);
+// console.log("The Car Brand Is :"+Cars[4]);
+
+for (let i = 0; i<5; i++) {
+
+    console.log("The Car Brand Is : "+Cars[i]);
+   
 }
-//console.log(cours);
-// console.log(course.views);
-// course.updateView();
-// console.log(course.views);
-
-function Course(title,teacher,level,isActive,views,updateView) {
-    
-    this.title=title,
-    this.teacher=teacher,
-    this.level=level,
-    this.isActive=isActive,
-    this.views=views,
-    this.updateView=function() {
-        return ++this.views;
-    }
-}
-
-
-var course1=new Course("JavaScript","Mohammad Hashemi",1,true,0);
-var course2=new Course("Materialize","qasem Bassaki",2,true,100);
-var course3=new Course("Html Css","Milad Dehyami",2,true,200);
-
-console.log(course1.title);
-console.log(course2.teacher);
-console.log(course3);
-
-
-
