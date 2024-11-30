73457261a851503efb20c3ef84b0170733213acc
ali <ali_babaie2005@yahoo.com>
2024-11-30 10:44:44 +0330

    section6 session11

--- Scripts.js
+++ Scripts.js
@@ -19,8 +19,30 @@ var course={
     }
 }
 //console.log(cours);
-console.log(course.views);
-course.updateView();
-console.log(course.views);
+// console.log(course.views);
+// course.updateView();
+// console.log(course.views);
+
+function Course(title,teacher,level,isActive,views,updateView) {
+    
+    this.title=title,
+    this.teacher=teacher,
+    this.level=level,
+    this.isActive=isActive,
+    this.views=views,
+    this.updateView=function() {
+        return ++this.views;
+    }
+}
+
+
+var course1=new Course("JavaScript","Mohammad Hashemi",1,true,0);
+var course2=new Course("Materialize","qasem Bassaki",2,true,100);
+var course3=new Course("Html Css","Milad Dehyami",2,true,200);
+
+console.log(course1.title);
+console.log(course2.teacher);
+console.log(course3);
+
 
 
