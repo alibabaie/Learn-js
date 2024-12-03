4760ae81c68791c8ad75ebf57f11a2d4de2af79c
ali <ali_babaie2005@yahoo.com>
2024-12-03 09:19:05 +0330

    section9 session23

--- Scripts.js
+++ Scripts.js
@@ -1,14 +1,47 @@
 
-const DIV = document.getElementById("demo");
+const ELEMENT=document.getElementById("myBtn");
 
-DIV.onmouseover = function (){
-   
-    document.getElementById("demo").style.backgroundColor="red";
+//  test by anonymous function
+// ELEMENT=addEventListener('click',function(){
 
-}
+//     document.getElementById("test").innerHTML=Date();
 
-DIV.onmouseout = function (){
+// });
 
-    document.getElementById("demo").style.backgroundColor="orange";
+//  test by name function
+// function displayDate(){
+
+// document.getElementById("test").innerHTML=Date();
+
+// }
+
+// ELEMENT.addEventListener('click',myFunction);
+
+// // ELEMENT.addEventListener('click',otherFunction);
+
+// function myFunction(){
+
+// alert("First Function Happend");
+
+// }
+
+// function otherFunction(){
+
+//     alert("Other Function Happend");
+    
+//     }
+
+let p1=7;
+let p2=4;
+
+ELEMENT.addEventListener('click',function(){
+
+myFunction(p1,p2);
+
+});
+
+function myFunction(a,b){
+
+document.getElementById("test").innerHTML=a*b;
 
 }
