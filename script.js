ad193de755d26763da3260bf4b350e279a86eaeb
ali <ali_babaie2005@yahoo.com>
2024-12-04 13:15:27 +0330

    section10 session27

--- script.js
+++ script.js
@@ -1,44 +1,107 @@
-const theTimer=document.querySelector(".timer");
-const testArea=document.querySelector("#test-area");
+const theTimer = document.querySelector(".timer");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p").innerHTML;
+const testWrapper=document.querySelector(".test-wrapper");
+const resetButton=document.querySelector("#reset");
+
+
 
 var timer=[0,0,0,0];
+var timerRunnig=false;
+var interval;
 
-var timerRuning=false;
 
-function leadinZero(time){
+function leadingZero(time){
 
-    if(time<=9){
-        time="0"+time;
+    if(time<=9)
+    {
+     time="0"+time;
     }
-    return time;
 
+    return time;
 }
+
 
 function runTimer(){
+let currentTime= leadingZero(timer[0])+":"+leadingZero(timer[1])+":"+leadingZero(timer[2]);
 
- let currentTime=leadinZero(timer[0])+":"+leadinZero(timer[1])+":"+leadinZero(timer[2]);
- 
- theTimer.innerHTML=currentTime;
+theTimer.innerHTML=currentTime;
 
- timer[3]++;
+timer[3]++;
 
- timer[0]=Math.floor((timer[3]/100)/60);
+timer[0]=Math.floor((timer[3]/100)/60);
+timer[1]=Math.floor(timer[3]/100)-(timer[0]*60);
+timer[2]=Math.floor(timer[3] - (timer[1]*100) - (timer[0]*6000));
 
- timer[1]=Math.floor((timer[3]/100))-(timer[0]*60);
 
- timer[2]=Math.floor(timer[3]-(timer[1]*100)-(timer[0]*6000));
-    
+
 }
 
-function Start(){
 
-    let textEnteredLength=testArea.value.length;
 
-    if(textEnteredLength==0 && !timerRuning)
+function spellCheck(){
+let textEntered=testArea.value;
+let originTextMatch=originText.substring(0,textEntered.length);
+
+
+if(textEntered==originText)
+{
+
+testWrapper.style.borderColor="green";
+clearInterval(interval);
+
+}else{
+    if(textEntered==originTextMatch){
+
+    testWrapper.style.borderColor="yellow";
+  
+    }
+    else
     {
-        timerRuning=true;
-        setInterval(runTimer,10);
+        
+        testWrapper.style.borderColor="red";
     }
 }
+}
+
+
+function reset(){
+    clearInterval(interval);
+    interval=null;
+    timer=[0,0,0,0];
+    timerRunnig=false;
+
+
+    testArea.value="";
+    theTimer.innerHTML="00:00:00";
+    testWrapper.style.borderColor="grey";
+
+}
+
+
+function Start(){
+  
+    let textEnteredLength=testArea.value.length;
+
+    if(textEnteredLength==0 && !timerRunnig)
+    {
+        timerRunnig=true;
+     interval = setInterval(runTimer,10);
+        
+    }
+    
+
+
+}
+
+
+
 
 testArea.addEventListener("keypress",Start);
+testArea.addEventListener("keyup",spellCheck);
+resetButton.addEventListener("click",reset);
+
+
+
+
+
