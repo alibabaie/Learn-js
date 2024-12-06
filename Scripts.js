c15d0152dca1128126a38484461dcf75e92a152c
ali <ali_babaie2005@yahoo.com>
2024-12-06 18:58:47 +0330

    section11 session32

--- Scripts.js
+++ Scripts.js
@@ -1,37 +1,40 @@
+//Range Error.....
 
-// try{
-//     console.log("Start of Try");
-
-//     uniercycle();
-
-//     console.log("End Of My Try --- Never Run");
-    
-    
-// }catch(error){
-//     console.log("Error is: "+error);
-    
-// }finally{
-//     console.log("Finally Always Run");
-// }
+// const array=[90,44];
+// Array.length=90**88;
 
 
-let json='{"age":30}';
+//Reference Error.....
 
-try{
-    let user=JSON.parse(json);
+// const red="red";
+// red;
+// blue;
 
-    //console.log(user);
 
-    if(!user.name)
-    {
-        throw "The json data is incomplete";
-        //throw new Error("The json data is incomplete");
-        //throw new SyntaxError("The json data is incomplete");
-    }
-    
-}catch(e){
-    console.log("Json Error  : "+e);
-    //console.log(e);
-    
-    
+
+//Syntax Error.....
+
+//let cat        h="cat";
+
+
+//Type Error.....
+
+//const number=123;
+//number.toUpperCase();
+
+// var hello="hello";
+// hello=hello.toUpperCase();
+// console.log(hello);
+
+
+//Internal Error.....
+
+function foo(){
+
+    foo()
 }
+
+foo();
+
+
+
