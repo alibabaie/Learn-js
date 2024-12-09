02fb0d5612a62c5b6d0dbb86c031d22c35529d07
ali <ali_babaie2005@yahoo.com>
2024-12-09 16:00:57 +0330

    section14 session41

--- script.js
+++ script.js
@@ -1,47 +1,19 @@
-//forEach تابع.....................
+function addPost(e) {
+  e.preventDefault();
 
+  let TitleValue = document.getElementById("title").value;
+  let BodyValue = document.getElementById("body").value;
 
-//  const numbers = [65,44,32,4];
-
-//  numbers.forEach(function(item,index,arr){
-
-//    console.log("item value is = "+item);
-
-//    console.log(`item = ${item} index = ${index}`);
-//    console.log(arr);
-  
-//  }
-
-//  );
-
-//Fetch Api......................................
-
-function getUsers(){
-
-fetch("users.json")
-// .then((res) => console.log(res));
-.then((res) => res.json())
-//.then((json) => console.log(json));
-.then((json) => {
-
-let output = "";
-
-json.forEach(function(user){
-
-output +=`<ul>
-
-<li>${user.id}</li>
-<li>${user.name}</li>
-<li>${user.email}</li>
-
-</ul>`;
-
-  });
-
-document.getElementById("output").innerHTML=output;
-
-});
-
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title: TitleValue, body: BodyValue }),
+  })
+    .then((res) => res.json())
+    .then((json) => console.log(json));
 }
 
-getUsers();
+document.getElementById("addPost").addEventListener("submit", addPost);
