ad35ef2031b9e4708266b77b390ef31c73e963fa
ali <ali_babaie2005@yahoo.com>
2024-12-11 10:10:29 +0330

    section15 session43

--- script.js
+++ script.js
@@ -21,30 +21,78 @@
 // userInfo("Ali",'Babaie',greeting);
 
 
-//Promise..............................
+//Promise.............................. Example 1
 
-const hasMeeting = false;
+// const hasMeeting = false;
 
-const meeting = new Promise ((resolve,reject) => {
+// const meeting = new Promise ((resolve,reject) => {
 
-if(!hasMeeting){
-  const meetingdetails = {
-    name:"Family Meeting",
-    location:"Babol",
-    time:"10:00 AM"
+// if(!hasMeeting){
+//   const meetingdetails = {
+//     name:"Family Meeting",
+//     location:"Babol",
+//     time:"10:00 AM"
 
-  };
-  resolve(meetingdetails);
-}else{
-reject(new Error ("Meeting Canceled"));
+//   };
+//   resolve(meetingdetails);
+// }else{
+// reject(new Error ("Meeting Canceled"));
+
+// }
+// });
+
+// const addToCalendar = meetingdetails => {
+
+//   const calendar = `${meetingdetails.name} is schedled at ${meetingdetails.location} on ${meetingdetails.time}`;
+//   return Promise.resolve(calendar);
+
+// };
+
+// meeting.then(addToCalendar)
+// .then((res) => console.log(res))
+// .catch((rej) => console.log(rej))
+
+
+//Promise.............................. Example 2
+
+// const Promise1 = Promise.resolve("promise 1 complete");
+
+// const Promise2 = new Promise((res,rej) => {
+
+//   setTimeout(() => {
+// res("promise 2 complete");
+//   },2000);
+
+// });
+
+// // Promise1.then((res) => console.log(res));
+// // Promise2.then((res) => console.log(res));
+
+// Promise.all([Promise1,Promise2]).then(res => console.log(res));
+
+
+
+//Promise.............................. Example 3
+
+
+function getMessage (callBack){
+
+setTimeout(function () {callBack("Hello callBack")
+  
+},2000);
 
 }
 
-});
+function myFunction(message) {
 
-meeting
-.then((res) => console.log(res))
-.catch((rej) => console.log(rej))
+  console.log(message);
+  
+}
+getMessage(myFunction);
+
+
+
+
 
 
 
