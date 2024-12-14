a1f9e64f17d7697f0d008b9f4ffac2c6c5971758
ali <ali_babaie2005@yahoo.com>
2024-12-14 09:14:11 +0330

    section18 session48

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
-
-const input = document.getElementById('name');
-const form = document.querySelector('form');
-const submit = document.getElementById('submitName');
-const remove = document.getElementById('removeName');
-
-form.addEventListener("submit",(e) => {
-
-e.preventDefault();
-
-});
-
-submit.addEventListener('click',() =>{
-
-sessionStorage.setItem('name',input.value);
-
-});
-
-remove.addEventListener('click',() =>{
-
-    sessionStorage.removeItem('name');
-
-});
-
-const h1 = document.querySelector('#title');
-
-const firstName = sessionStorage.getItem('name');
-
-firstName
- ? (h1.innerText=`Welcome ${firstName}`) : (h1.innerText='Nobody To Welcome');
+const btn=document.getElementById("btnSave");
 
 
+btn.addEventListener('click',()=>{
+
+    const myObject={firstName:"Ali",lastName:"Babaie",isMale:true};
+
+    var myArray=[
+    {firstName:"Ali",lastName:"Babaie",isMale:true},
+    {firstName:"Amir Hossain",lastName:"Babaie",isMale:true},
+    {firstName:"Zeynab",lastName:"Babaie",isMale:false},
+    {firstName:"Fatemeh",lastName:"Farajpoor",isMale:false},
+];
 
 
+    localStorage.setItem('PersonInfo',JSON.stringify(myArray));
+
+
+})
