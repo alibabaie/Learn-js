c0c7b88fa41ddb62c96887b01cd2fa491df532c7
ali <ali_babaie2005@yahoo.com>
2024-12-15 23:42:31 +0330

    section19 session50

--- TodoProApp/js/scripts.js
+++ TodoProApp/js/scripts.js
@@ -4,7 +4,6 @@ const addBtn = document.getElementById("add-btn");
 const todoInput = document.getElementById("addt");
 
 function main() {
-
   // Theme-Switcher
   themeSwitcherBtn.addEventListener("click", () => {
     bodyTag.classList.toggle("light");
@@ -17,6 +16,9 @@ function main() {
     );
   });
 
+
+  makeTodoElement(JSON.parse(localStorage.getItem("todos")));
+
   //Add Todo In LocalStorage
   addBtn.addEventListener("click", () => {
     const item = todoInput.value.trim();
@@ -26,15 +28,56 @@ function main() {
         ? []
         : JSON.parse(localStorage.getItem("todos"));
 
-        const currentTodo={
-          item:item,
-          isCompleted:false
-        }
+      const currentTodo = {
+        item: item,
+        isCompleted: false,
+      };
 
-        todos.push(currentTodo);
-        localStorage.setItem("todos",JSON.stringify(todos));
-
+      todos.push(currentTodo);
+      localStorage.setItem("todos", JSON.stringify(todos));
     }
+  });
+}
+
+function makeTodoElement(todoArray) {
+  if (!todoArray) {
+    return null;
+  }
+
+  todoArray.forEach((todoObject) => {
+    //Create Html Elements Of Todo
+    const card = document.createElement("li");
+    const cbContainer = document.createElement("div");
+    const cbInput = document.createElement("input");
+    const checkSpan = document.createElement("span");
+    const item = document.createElement("p");
+    const clearBtn = document.createElement("button");
+    const img = document.createElement("img");
+
+    //Add Classes
+    card.classList.add("card");
+    cbContainer.classList.add("cb-container");
+    cbInput.classList.add("cb-input");
+    checkSpan.classList.add("check");
+    item.classList.add("item");
+    clearBtn.classList.add("clear");
+    //Add Attributes
+    card.setAttribute("draggable", true);
+    cbInput.setAttribute("type", "checkbox");
+    img.setAttribute("src", "./assets/images/icon-cross.svg");
+    img.setAttribute("alt", "Clear It");
+    item.textContent = todoObject.item;
+    //Add EventListener
+
+    //Set Element by Parent Child
+    clearBtn.appendChild(img);
+    cbContainer.appendChild(cbInput);
+    cbContainer.appendChild(checkSpan);
+    card.appendChild(cbContainer);
+    card.appendChild(item);
+    card.appendChild(clearBtn);
+
+    document.querySelector(".todos").appendChild(card);
   });
 }
 
