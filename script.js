97d4218d363931e9f5a21036c26648301bf3a445
ali <ali_babaie2005@yahoo.com>
2024-12-16 04:03:15 +0330

    section19 session51

---
+++ script.js
@@ -0,0 +1,85 @@
+// #region Spread Opeator
+
+///////////////Spread Opeator/////////////////////
+
+ //const Cars=["BMW","Benz","Ferrari","Peykan","Pride"];
+
+ //const names=["Mohammad","Ali","Reza","Milad",...Cars];
+
+ //console.log(names);
+ //console.log(names[0]+" "+names[1]+" "+names[2]+" "+names[3]);
+ //console.log(...names);
+ 
+ 
+
+
+//  function sum(x,y,z){
+//      console.log(x+y+z);
+//  }
+
+
+//  var args=[0,1,2];
+
+//  sum(args[0],args[1],args[2]);
+
+//  sum(...args);
+
+
+
+//  var array1=[1,2,3];
+
+//  var array2=[...array1];
+
+//  array2.push(4);
+
+//  console.log(array1);
+//  console.log(array2);
+
+
+
+
+
+
+
+// #endregion
+
+
+
+// #region FilterMethodInArray
+
+//  const array=[12,5,87,8,130,44];
+
+//  console.log(array.filter((value,index)=>{
+
+//      return index >1;
+
+//  }));
+
+
+// #endregion
+
+
+// #region IndexOf
+
+
+//  const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+
+//  console.log(fruits.indexOf("Mango"));
+
+
+// #endregion
+
+
+
+// #region Splice
+
+const myFish=['angle','clown','mandarian','whale'];
+
+ myFish.splice(3,1,"Fish1","Fish2");
+
+ console.log(myFish);
+
+// #endregion
+
+
