13d54998c54ee55ec9794dfe47a51107614ffb3a
ali <ali_babaie2005@yahoo.com>
2024-12-23 17:42:05 +0330

    section20 session56

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-
 let db;
 const firstName = document.querySelector("#firstName");
 const lastName = document.querySelector("#lastName");
-const form = document.querySelector("form");
+const from = document.querySelector("form");
 
 window.onload = () => {
-
     let request = window.indexedDB.open("contacts", 1);
 
     request.onerror = () => {
-        console.log("Database Faild To Open");
-
+        console.log("Database Failed To Open");
     }
+
+
     request.onsuccess = () => {
         console.log("Database Opened Successfully");
+        db = request.result;
+        console.log(db);
+    }
+
+
+    request.onupgradeneeded = (e) => {
+        let db = e.target.result;
+        console.log(db);
+
+        let objectStore = db.createObjectStore('contacts', {
+            keyPath: 'id',
+            autoIncrement: true
+        });
+
+
+        objectStore.createIndex('firstName', {
+            unique: false
+        });
+
+        objectStore.createIndex('lastName', {
+            unique: false
+        });
+
+
+        console.log("Database Setup Successfully...");
 
     }
-    db = request.result;
+
+
+
+
+
+
+
+
 }
