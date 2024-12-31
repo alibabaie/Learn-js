888b3140adff9cf16ce74470b4f38b23d1d50d65
ali <ali_babaie2005@yahoo.com>
2024-12-31 11:59:52 +0330

    section22 session72

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function Car(name, speed) {
   this.name = name;
 
-   let bodyWeight = 550;
+  let bodyWeight = 550;
 
   let calculateSpeed = function () {
     bodyWeight / 12;
@@ -11,11 +11,27 @@ function Car(name, speed) {
     calculateSpeed();
     console.log(speed);
   };
+
+  Object.defineProperty(this,'bodyWeight',{
+    get: function() {
+      return bodyWeight;
+    },
+    set: function(value) {
+      if(value <= 0){
+        throw new Error("Invalid Number");
+      }
+
+      bodyWeight=value+10;
+    }
+
+  })
+
 }
 
 const pride = new Car("killer machine", 180);
+pride.bodyWeight=220;
+console.log(pride.bodyWeight);
 
-pride.maxSpeed();
 
 
 
