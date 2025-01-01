e0932b9629fc81710837a3044bc06ac176736d3b
ali <ali_babaie2005@yahoo.com>
2025-01-01 15:39:36 +0330

    section22 session79

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-doSomthing();
 
-doSomthing2();
+class User {
 
+    constructor (username,email) {
 
+        this.username = username;
+        this.email = email;
+    }
 
+    // Instance Methods......................
+    getInfo () {
 
+        return `نام کاربری : ${this.username} , ایمیل : ${this.email}`;
+    }
 
-// Function Declaration..........................
+    // Stattic Methods.......................
 
-function doSomthing () {}
+    static validateEmail (email) {
 
+        if (!email || !email.trim() == '') return false;
+        if (!email.includes('@')) return false;
 
-// Function Expression...........................
+        return true;
+    }
+}
 
-const doSomthing2 = function () {}
+const user1 = new User ("ali babaie",'ali@gmail.com');
+const user2 = new User ("amir hossain babaie",'amir hossain@gmail.com');
+
+user1.getInfo();
+user2.getInfo();
+//user1.validateEmail();
